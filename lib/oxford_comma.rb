dbe3f9c8808d5bcef6fbf80e16a2aa3bd0c47c6c
Rina Kawamura <rina.kawamura@yale.edu>
2019-04-29 05:25:03 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,12 @@
 def oxford_comma(array)
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return array.join(" and ")
+  else
   last = array.pop
   final = array.join(", ")
   final << ", and #{last}"
   return final
+  end
 end
